8a6206af6febaaa6b9d1df51ca80d5d94a6b0620
Melina Alejos <malejos1219@gmail.com>
2020-08-04 11:17:02 -0400

    first commit, working project need to fix my API

---
+++ src/main/java/com/tts/transitapp/controller/TransitController.java
@@ -0,0 +1,34 @@
+package com.tts.transitapp.controller;
+
+import java.util.List;
+
+import com.tts.transitapp.model.Bus;
+import com.tts.transitapp.model.BusRequest;
+import com.tts.transitapp.service.TransitService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class TransitController {
+    @Autowired
+    private TransitService apiService;
+
+    @GetMapping("/buses")
+    public String getBusesPage(Model model) {
+        BusRequest busRequest = new BusRequest();
+        model.addAttribute("request", busRequest);
+        return "index";
+    }
+
+    @PostMapping("/buses")
+    public String getNearbyBuses(BusRequest request, Model model) {
+        List<Bus> buses = apiService.getNearbyBuses(request);
+        model.addAttribute("buses", buses);
+        model.addAttribute("request", request);
+        return "index";
+    }
+}
