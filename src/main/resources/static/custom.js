ad544ce59015896a6bf8b77d6d697364313b0be1
Melina Alejos <malejos1219@gmail.com>
2020-08-04 15:32:34 -0400

    added user location and map centers aroud user location

--- src/main/resources/static/custom.js
+++ src/main/resources/static/custom.js
@@ -1,4 +1,4 @@
-var map;
+var map, infoWindow;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: parseFloat(busLocations[0].LATITUDE), lng: parseFloat(busLocations[0].LONGITUDE) },
@@ -6,10 +6,42 @@ function initMap() {
         scrollwheel: false
     });
 
+    var iconBase = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/';
+
     for (i = 0; i < busLocations.length; i++) {
         var marker = new google.maps.Marker({
             position: { lat: parseFloat(busLocations[i].LATITUDE), lng: parseFloat(busLocations[i].LONGITUDE) },
             map: map,
+            // icon: iconBase + 'bus_stop_maps.png'
         });
     }
+
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function () {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
